da2601b619b8d458e9da1ad99102acf2aa71447d
Raven.sun <raven.sun@ratchet.cloud>
2019-07-02 10:44:22 +0800

    add @ function, support search room

--- autoText.js
+++ autoText.js
@@ -1,3 +1,12 @@
+const moment = require('moment');
+
+const getDuration = () => {
+  const releaseDate = moment('2019-08-27');
+  const now = moment();
+  const duration = releaseDate.diff(now, 'days');
+  return duration;
+};
+
 module.exports = [
   {
     key: '什么时候开服',
@@ -5,6 +14,16 @@ module.exports = [
   },
   {
     key: '公会制度',
-    text:'https://img.nga.178.com/attachments/mon_201906/25/8iQ5-bum6XkZ5gT3cS1hq-35s.png',
+    text:
+      'https://img.nga.178.com/attachments/mon_201906/25/8iQ5-bum6XkZ5gT3cS1hq-35s.png'
+  },
+  {
+    key: '倒计时',
+    text: () => {
+      return `距离开服还有${getDuration()}天`;
+    },
+    callback: async (room) => {
+      await room.topic(`Evil Altar - ${getDuration()}天`)
+    }
   }
 ];
--- mybot.js
+++ mybot.js
@@ -3,14 +3,17 @@ const { FileBox } = require('file-box');
 const qrTerm = require('qrcode-terminal');
 const autoText = require('./autoText.js');
 
-const bot = new Wechaty({ name: 'ea-bot' });
+const roomRegex = /^Evil Altar*/;
 
-// const eaRoomID =
-//   '@@5bf05729d35be05b847c5eed97319fc0c9fe777dfecb89e54e1f479bfb386794';
+let eaRoom = {};
 
 const handleScan = (qrcode, status) => {
   qrTerm.generate(qrcode, { small: true }); // show qrcode on console
-  console.log(`https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(qrcode)}`);
+  console.log(
+    `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(
+      qrcode
+    )}`
+  );
 };
 
 const handleLogin = user => {
@@ -25,33 +28,63 @@ const handleError = e => {
   console.error(e);
 };
 
-const getAutoReply = message => {
-  let result = null;
-  const currentTextObj = autoText.find(item => item.key === message);
-  if (!currentTextObj) return null;
-  if (currentTextObj.file) {
-    result = currentTextObj.file;
-  } else {
-    result = currentTextObj.text;
-  }
-  return result;
-};
-
 const handleMessage = async msg => {
   const contact = msg.from();
   const text = msg.text();
   const room = msg.room();
   if (!room) return;
-  // if (room.id !== eaRoomID) return;
-  const autoReply = getAutoReply(text);
-  if (autoReply) {
-    msg.say(`自动回复:${autoReply}`);
+  if (room.id !== eaRoom.id) return;
+  if (await msg.mentionSelf()) {
+    await msg.say(`自动回复: 别@我,有事@法神`);
+  }
+  const main = async () => {
+    const current = autoText.find(item => item.key === text);
+    if (!current) return;
+    if (current.callback) {
+      await current.callback(room, msg);
+    }
+    if (current.text) {
+      if (typeof current.text == 'function') {
+        await msg.say(`自动回复: ${current.text()}`);
+      } else {
+        await msg.say(`自动回复: ${current.text}`);
+      }
+    }
+    if (current.file) {
+      const fileBox = fileBox(current.file);
+      await msg.say(fileBox);
+    }
+  };
+  await main();
+};
+
+const main = async () => {
+  const bot = new Wechaty({ name: 'ea-bot' });
+  bot.on('scan', handleScan);
+  bot.on('login', handleLogin);
+  bot.on('logout', handleLogout);
+  bot.on('error', handleError);
+  bot.on('message', handleMessage);
+  await bot.start();
+
+  const searchTopic = process.argv[2];
+
+  // wait the bot for logging in
+  while (!bot.logonoff()) {
+    await new Promise(r => setTimeout(r, 100));
+  }
+
+  const room = await bot.Room.find({ topic: roomRegex });
+
+  if (room) {
+    const { id } = room;
+    eaRoom = { id };
+    const topic = await room.topic();
+    console.log(`room topic is : ${topic}`);
   }
 };
 
-bot.on('scan', handleScan);
-bot.on('login', handleLogin);
-bot.on('logout', handleLogout);
-bot.on('error', handleError);
-bot.on('message', handleMessage);
-bot.start();
+main().catch(e => {
+  console.error(e);
+  process.exit(1);
+});
